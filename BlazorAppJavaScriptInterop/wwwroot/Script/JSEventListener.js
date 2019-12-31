2ac6ed88c5cb4bf6f9e447dbb37d19cbeb52390f
naveen <naveen@MS-00603>
2019-12-31 12:19:10 +0530

    ADDED: JavaScript Interop Demo.

---
+++ BlazorAppJavaScriptInterop/wwwroot/Script/JSEventListener.js
@@ -0,0 +1,24 @@
+﻿function RegisterEvent(Element)
+{
+    var ButtonElement = document.getElementById('AlertButton');
+    ButtonElement.addEventListener("click", ShowAlert);
+
+    Element.addEventListener('mouseover',
+        function ChangeColorOnMouseOver()
+        {
+            Element.style.color = "chartreuse";
+            Element.style.fontWeight = "bolder";
+        });
+
+    Element.addEventListener('mouseout',
+        function ChangeColorToNormal()
+        {
+            Element.style.color = "black";
+            Element.style.fontWeight = "normal";
+        });
+}
+
+function ShowAlert()
+{
+    alert('Alert triggered from Button');
+}
---
+++ BlazorAppJavaScriptInterop/wwwroot/Script/RenderHTMLElement.js
@@ -0,0 +1,7 @@
+﻿function CreateDynamicHTMLElement()
+{
+    var dynamicElement = document.createElement("h5");
+    dynamicElement.innerHTML = "This Element created from JS Interop with styles";
+    dynamicElement.setAttribute('style', "color: darkseagreen; font-weight:bolder");
+    document.getElementById('DivContainer').appendChild(dynamicElement);
+}
