3c7be25ca89f2846cbbbbf272d728b075db42a24
naveen <naveen@MS-00603>
2020-01-03 18:38:13 +0530

    ADDED: Dynamic component  solution.

---
+++ BlazorInsuranceApp/Model/EDINS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorInsuranceApp
+{
+	public class EDINS
+	{
+		#region Properties
+		public Guid EDINSId { get; set; }
+
+		[Required]
+		public int InsSubNr { get; set; }
+
+		public string Abbreviation { get; set; }
+
+		[Required]
+		public string ShortCaption { get; set; }
+
+		[Required]
+		public string Caption { get; set; }
+
+		[Required]
+		public string InsuranceType { get; set; }
+		#endregion
+	}
+}
---
+++ BlazorInsuranceApp/Model/EDPLN.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorInsuranceApp.Model
+{
+	public class EDPLN
+	{
+		#region Properties
+		public Guid EDPLNId { get; set; }
+		public Guid EDINSId { get; set; }
+
+		public string Abbreviation { get; set; }
+
+		[Required]
+		public string ShortCaption { get; set; }
+
+		[Required]
+		public string Caption { get; set; }
+		#endregion
+	}
+}
