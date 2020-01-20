c8d378cd5b1640819205aa02cd4f794f22bc33b0
naveen <naveen@MS-00603>
2020-01-20 12:00:08 +0530

    ADDED: New solution for form validation

---
+++ BlazorFormValidation/EDInsurance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFormValidation
+{
+	public class EDInsurance
+	{
+		#region Properties
+		public Guid InsuranceId { get; set; }
+
+		[Required(ErrorMessage = "Insurance Name is Required")]
+		[StringLength(10, ErrorMessage = "Name should be less than 10 characters")]
+		public string InsuranceName { get; set; }
+
+		[Required(ErrorMessage = "Caption is required")]
+		public string Caption { get; set; }
+
+		[Required]
+		public bool IsEmployeeExists { get; set; }
+
+		[Required(ErrorMessage = "Date of birth is required")]
+		public DateTime DOB { get; set; }
+
+		[Required]
+		public int Code { get; set; }
+		#endregion
+	}
+}
---
+++ BlazorFormValidation/Pages/TableData.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BlazorInsuranceApp
+{
+	public class TableData
+	{
+		#region Fields
+		private static SqlConnection connection;
+		private static DataTable tabAnonymous;
+		private static SqlDataAdapter dataAdapter;
+		//private static SqlDataReader dataReader;
+		#endregion
+
+		#region Publics
+		public static DataTable GetInsurance(string strTableName)
+		{
+			using(connection = new SqlConnection("Data Source=MS-00603;Initial Catalog=BlazorEducation;Persist Security Info=True;User ID=sa;Password=password-123"))
+			{
+				string sqlQuery = $"select * from {strTableName} where InsSubNr = 10006 ";
+				connection.Open();
+
+				dataAdapter = new SqlDataAdapter(sqlQuery, connection);
+				tabAnonymous = new DataTable();
+
+				dataAdapter.Fill(tabAnonymous);
+			}
+
+			return tabAnonymous;
+		}
+		#endregion
+	}
+}
