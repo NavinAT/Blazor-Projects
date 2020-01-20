c9bb944af4f5b178900d64bb72a2805d8759cbdd
naveen <naveen@MS-00603>
2020-01-20 16:49:34 +0530

    ADDED: State Management solution.

---
+++ BlazorDemo/BlazorStateManagement/CounterService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BlazorStateManagement
+{
+	public class CounterService
+	{
+		#region Events
+		public event EventHandler StateChanged;
+		#endregion
+
+		#region Properties
+		public int Count { get; private set; }
+		#endregion
+
+		#region Publics
+		public int GetCurrentCount()
+		{
+			return this.Count;
+		}
+
+		public void Reset()
+		{
+			this.Count = 0;
+			StateHasChanged();
+		}
+
+		public void SetCurrentCount(int nParamCount)
+		{
+			this.Count = nParamCount;
+			StateHasChanged();
+		}
+		#endregion
+
+		#region Privates
+		private void StateHasChanged()
+		{
+			this.StateChanged?.Invoke(this, EventArgs.Empty);
+		}
+		#endregion
+	}
+}
