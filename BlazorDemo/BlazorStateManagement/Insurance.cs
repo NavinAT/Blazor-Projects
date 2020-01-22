d4b4529a7176574a6ae296f5998651559a00dd97
naveen <naveen@MS-00603>
2020-01-22 18:19:50 +0530

    ADDED: Projects for CascadingValue, Layouts, StateManagement, ErrorHandling.

---
+++ BlazorDemo/BlazorStateManagement/Insurance.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorStateManagement
+{
+	public class Insurance
+	{
+		#region Properties
+		public string InsuranceName { get; set; }
+		public string Caption { get; set; }
+		public int InsuranceCode { get; set; }
+		#endregion
+	}
+}
--- BlazorDemo/ErrorHandling/Startup.cs
+++ BlazorDemo/ErrorHandling/Startup.cs
@@ -1,33 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ErrorHandling.Data;
 
-namespace BlazorStateManagement
+namespace ErrorHandling
 {
 	public class Startup
 	{
-		#region Properties
-		public IConfiguration Configuration { get; }
-		#endregion
-
-		#region Constructors
 		public Startup(IConfiguration configuration)
 		{
-			this.Configuration = configuration;
+			Configuration = configuration;
 		}
-		#endregion
 
-		#region Publics
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<CounterService>();
-			services.AddProtectedBrowserStorage();
+			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,11 +51,10 @@ namespace BlazorStateManagement
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints =>
-			                 {
-				                 endpoints.MapBlazorHub();
-				                 endpoints.MapFallbackToPage("/_Host");
-			                 });
+			{
+				endpoints.MapBlazorHub();
+				endpoints.MapFallbackToPage("/_Host");
+			});
 		}
-		#endregion
 	}
 }
