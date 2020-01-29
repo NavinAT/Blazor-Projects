9a2fdd51e8c6804d0414881c8733f37c9e2e7ce4
naveen <naveen@MS-00603>
2020-01-29 11:49:31 +0530

    CHANGED: New port has been configured for kestrel server

--- BlazorDemo/BlazorKestrelSample/Program.cs
+++ BlazorDemo/BlazorKestrelSample/Program.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace BlazorKestrelSample
@@ -8,12 +11,37 @@ namespace BlazorKestrelSample
 		#region Publics
 		public static void Main(string[] args)
 		{
+			CreateWebHostBuilder(args).Build().Run();
+
 			CreateHostBuilder(args).Build().Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 			    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+		// Configuring port number via code
+
+		//public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+		//	WebHost.CreateDefaultBuilder(args)
+		//	       .UseUrls("http://localhost:6000")
+		//	       .UseKestrel()
+		//	       .UseStartup<Startup>();
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		{
+			IConfigurationRoot configRoot = new ConfigurationBuilder()
+			                                .SetBasePath(Directory.GetCurrentDirectory())
+			                                .AddJsonFile("appsettings.json", optional: false)
+			                                .Build();
+
+			IWebHostBuilder webHost = WebHost.CreateDefaultBuilder(args)
+			                                 .UseUrls($"http://localhost:{configRoot.GetValue<int>("Host:Port")}")
+			                                 .UseKestrel()
+			                                 .UseStartup<Startup>();
+
+			return webHost;
+		}
 		#endregion
 	}
 }
